9d001fb2c23fc4cbb2c2582d798b9f94233e893f
deb <debkbanerji@gmail.com>
2016-06-28 21:41:56 -0400

    npm init
    Ran npm init, installed express and created server.js as well as app directory

---
+++ server.js
@@ -0,0 +1,72 @@
+/**
+ * Created by Deb Banerji on 26-Jun-16.
+ */
+var express = require("express");
+var os = require('os');
+const path = require('path');
+var credentials = require(path.join(__dirname, "credentials.json"));
+var db = credentials.Database;
+console.log("JSON Database: " + db);
+
+var app = express();
+
+var port = process.env.PORT || 3000;
+var database = process.env.DATABASE || db;
+console.log("Database: " + database);
+/*
+ To set a port other than 3000:
+ in Unix:
+
+ $ PORT=1234 node server.js
+
+ in Windows:
+
+ set PORT=1234
+ node server.js
+ */
+
+var ifaces = os.networkInterfaces();
+
+Object.keys(ifaces).forEach(function (ifname) {
+    var alias = 0;
+
+    ifaces[ifname].forEach(function (iface) {
+        if ('IPv4' !== iface.family || iface.internal !== false) {
+            // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+            return;
+        }
+
+        if (alias >= 1) {
+            // this single interface has multiple ipv4 addresses
+            console.log(ifname + ':' + alias, iface.address);
+        } else {
+            // this interface has only one ipv4 adress
+            console.log(ifname, iface.address);
+        }
+        ++alias;
+    });
+});
+
+app.use("/credentials.json",function (req, res, next) {
+    res.redirect("/")
+});
+
+// app.use("/credentials.js",function (req, res, next) {
+//     res.redirect("/")
+// });
+
+app.use("/server.js",function (req, res, next) {
+    res.redirect("/")
+});
+
+app.use(express.static(path.join(__dirname, "app")));
+
+
+app.all('*', function (req, res) {
+    res.redirect("/");
+});
+
+app.listen(port, function () {
+    console.log('listening on port ' + port);
+    // console.log('press Ctrl + C to shut down server');
+});
