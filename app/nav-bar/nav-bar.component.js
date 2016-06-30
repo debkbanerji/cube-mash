e37fd692e51f681312679b4a3f10f981703bde1f
deb <debkbanerji@gmail.com>
2016-06-30 10:04:42 -0400

    Responsive nav-bar
    Items in nav-bar now hide themselves based on url

--- app/nav-bar/nav-bar.component.js
+++ app/nav-bar/nav-bar.component.js
@@ -1,13 +1,30 @@
 angular.module('navBar').component('navBar', {
     templateUrl: 'nav-bar/nav-bar.template.html',
 
-    controller: ['$location', function navBarController($location) {
+    controller: ['$scope', '$location', function navBarController($scope, $location) {
         var self = this;
-        var def = "DEF";
-        var path = $location.path();
-        var url = $location.url();
-        console.log("location: ", $location);
-        console.log("url: ", url);
-        console.log("path: ", path);
-    }]
+        updateNavBar($location, self);
+
+        self.abcd = true;
+        
+        $scope.$on('$routeChangeSuccess', function () {
+            updateNavBar($location, self);
+        });
+    }
+
+    ]
 });
+
+function updateNavBar(location, self) {
+    self.path = location.path();
+    self.url = location.url();
+    console.log("location: ", location);
+    console.log("url:", self.url);
+    console.log("path:", self.path);
+    self.mash = /mash$/.test(self.path);
+    self.stats = /stats$/.test(self.path);
+    self.about = /about$/.test(self.path);
+    console.log(self.mash);
+    console.log(self.stats);
+    console.log(self.about);
+}
