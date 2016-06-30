a906a4ad9e640506505b53d2f1788cd9db1494db
deb <debkbanerji@gmail.com>
2016-06-29 21:59:32 -0400

    'about' Placeholder
    Created placeholder 'about' module

---
+++ app/about/about.component.js
@@ -0,0 +1,10 @@
+// Register `phoneList` component, along with its associated controller and template
+angular.
+module('about').
+component('about', {
+    templateUrl: 'about/about.template.html',
+
+    controller: function PhoneListController() {
+        var self = this;
+    }
+});
