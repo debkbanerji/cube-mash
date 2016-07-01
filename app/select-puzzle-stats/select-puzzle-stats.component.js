4bdc1414897298f13126e558a6252ff77c9926b5
deb <debkbanerji@gmail.com>
2016-07-01 09:05:31 -0400

    Stats Update
    Statistics page now shows relevant data

--- app/select-puzzle-stats/select-puzzle-stats.component.js
+++ app/select-puzzle-stats/select-puzzle-stats.component.js
@@ -1,10 +1,8 @@
-angular.
-module('selectPuzzleStats').
-component('selectPuzzleStats', {
+angular.module('selectPuzzleStats').component('selectPuzzleStats', {
     templateUrl: 'select-puzzle-stats/select-puzzle-stats.template.html',
 
     controller: function selectPuzzleStatsController($routeParams) {
         var self = this;
-        this.puzzleTypes = ["3x3", "2x2", "4x4","5x5","6x6","7x7","Megaminx","Pyraminx","Square-1", "Skewb", "Clock"];
+        this.puzzleTypes = ["3x3", "2x2", "4x4", "5x5", "6x6", "7x7", "Megaminx", "Pyraminx", "Square-1", "Skewb"];
     }
 });
--- app/stats/stats.component.js
+++ app/stats/stats.component.js
@@ -8,17 +8,5 @@ angular.module('stats').component('stats', {
         self.puzzlesRef = firebase.database().ref().child("puzzles").child(self.puzzleId);
         // putting a console.log here won't work due to the asynchronous call
         self.puzzles = $firebaseArray(self.puzzlesRef);
-        // download the data into a local object
-        // this.puzzles = $firebaseObject(ref);
-        // putting a console.log here won't work, see below
-        self.puzzles = $firebaseArray(ref);
-
-        // self.pushData = function pushData() {
-        //     self.puzzles.$add({
-        //         name: "Moyu Weilong GTS",
-        //         wins: 0,
-        //         losses: 0
-        //     });
-        // }
     }]
 });
