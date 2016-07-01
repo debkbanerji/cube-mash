827ddaf40fe436879b4f8b48d87222e171c899de
Deb Banerji <debkbanerji@gmail.com>
2016-07-01 16:18:12 +0530

    Stats Ordering
    Puzzles in stats are now ordered by win rate

--- app/face-off/face-off.component.js
+++ app/face-off/face-off.component.js
@@ -4,10 +4,11 @@ angular.module('faceOff').component('faceOff', {
     controller: ['$scope', '$routeParams', '$firebaseObject', '$firebaseArray', function faceOffController($scope, $routeParams, $firebaseObject, $firebaseArray) {
         var self = this;
         self.puzzleId = $routeParams.puzzleId;
-        // console.log("puzzleId:" + self.puzzleId);
         self.puzzlesRef = firebase.database().ref().child("puzzles").child(self.puzzleId);
         // putting a console.log here won't work due to the asynchronous call
         self.puzzles = $firebaseArray(self.puzzlesRef);
+
+        
 
         self.addWin = function pushData(puzzle) {
             var winsRef = self.puzzlesRef.child(puzzle.$id).child("wins");
--- app/stats/stats.component.js
+++ app/stats/stats.component.js
@@ -4,9 +4,12 @@ angular.module('stats').component('stats', {
     controller: ['$scope', '$routeParams', '$firebaseObject', '$firebaseArray', function statsController($scope, $routeParams, $firebaseObject, $firebaseArray) {
         var self = this;
         self.puzzleId = $routeParams.puzzleId;
-        console.log("puzzleId:" + self.puzzleId);
         self.puzzlesRef = firebase.database().ref().child("puzzles").child(self.puzzleId);
         // putting a console.log here won't work due to the asynchronous call
         self.puzzles = $firebaseArray(self.puzzlesRef);
+
+        $scope.winRate = function(puzzle) {
+            return (100*puzzle.wins/(puzzle.losses+puzzle.wins)||0);
+        };
     }]
 });
