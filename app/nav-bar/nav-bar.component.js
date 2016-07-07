cbc14889c4e0d06e37163cb426f08da119633cba
deb <debkbanerji@gmail.com>
2016-07-07 10:26:01 -0400

    Nav Bar Update
    Changed the way nav bar items are highlighted

--- app/nav-bar/nav-bar.component.js
+++ app/nav-bar/nav-bar.component.js
@@ -5,8 +5,6 @@ angular.module('navBar').component('navBar', {
         var self = this;
         updateNavBar($location, self);
 
-        self.abcd = true;
-        
         $scope.$on('$routeChangeSuccess', function () {
             updateNavBar($location, self);
         });
@@ -18,13 +16,7 @@ angular.module('navBar').component('navBar', {
 function updateNavBar(location, self) {
     self.path = location.path();
     self.url = location.url();
-    // console.log("location: ", location);
-    // console.log("url:", self.url);
-    // console.log("path:", self.path);
-    self.mash = /mash$/.test(self.path);
+    self.play = /play$/.test(self.path);
     self.stats = /stats$/.test(self.path);
     self.about = /about$/.test(self.path);
-    // console.log(self.mash);
-    // console.log(self.stats);
-    // console.log(self.about);
 }
